9b02042ccaca2b23072623dff33953937cfc9202
Rowan Oliver <r.oliver.yoga@gmail.com>
2021-01-01 22:45:26 -0800

    consistent errors, attempting different angle

--- Develop/index.js
+++ Develop/index.js
@@ -5,48 +5,73 @@ const util = require("util");
 
 const writeFileAsync = util.promisify(fs.writeFile)
 
-function Questions(){
+function InputQuestions()
     return inquirer.prompt ([
         {
             type:"input",
             name:"username",
-            message:"What is your GitHub Username?"
+            message:"What is your GitHub Username?",
         },
         {   type:"input",
             name:"title",
-            message:"Please enter the name of your prject"
+            message:"Please enter the name of your prject",
 
         },
         {
             type:"input",
             name:"description",
-            message:"Please enter a short description of your project"
+            message:"Please enter a short description of your project",
         },
         {
             type:"input",
             name:"license",
-            message:"What type of license does your project require?"
+            message:"What type of license does your project require?",
         },
         {
             type:"input",
             name:"installation",
-            message:"Are there any commands that should be run to install dependecies?"
+            message:"Are there any commands that should be run to install dependecies?",
         
         },
         {
             type:"input",
             name:"test",
-            message:"Please provide command for any test to be run"
+            message:"Please provide command for any test to be run",
         
         },
         {   type:"input",
              name:"contributing",
-            message:"Does the user need to contrubte to the repo?"
+            message:"Does the user need to contrubte to the repo?",
 
         },
 
 
-    ])
-};
+    
+//console.log everything 
+
+ 
+//how to use the data that the user input
+var getInput = (data) => {
+
+}
+
+
+
+
+// function to generate markdown for README// from the starter code:figure out what they do
+function generateMarkdown(data) {
+    return `# ${data.title}
+    ##Description
+    ${data.description}
+    ## Tabel of Contents
+    - [Questions](#questions)
+    -[License](#license)
+    - []
+  
+  `;
+  }
+  
+  module.exports = generateMarkdown;
+
 
 
