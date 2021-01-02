7e51cc9976afe8321aa599cfca2a2c9011f58166
Rowan Oliver <r.oliver.yoga@gmail.com>
2021-01-02 12:58:26 -0800

    IT WORKSgit add .

--- Develop/index.js
+++ Develop/index.js
@@ -1,77 +1,51 @@
-const fs = require('fs');
-const axios = require("axios");
+//create consts for things that are required 
 const inquirer = require("inquirer");
-const util = require("util");
+const fs = require("fs");
 
-const writeFileAsync = util.promisify(fs.writeFile)
+inquirer
+    .prompt([{
+        type: "input",
+        name: "username",
+        message: "What is your GitHub Username?",
+    },
+    {
+        type: "input",
+        name: "title",
+        message:" What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message:"Please enter a short description of your project",
 
-function InputQuestions()
-    return inquirer.prompt ([
-        {
-            type:"input",
-            name:"username",
-            message:"What is your GitHub Username?",
-        },
-        {   type:"input",
-            name:"title",
-            message:"Please enter the name of your prject",
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "What type of license doeyour project require?",
 
-        },
-        {
-            type:"input",
-            name:"description",
-            message:"Please enter a short description of your project",
-        },
-        {
-            type:"input",
-            name:"license",
-            message:"What type of license does your project require?",
-        },
-        {
-            type:"input",
-            name:"installation",
-            message:"Are there any commands that should be run to install dependecies?",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Are they any commands that should be run to install any dependencies?",
+
+    },
+    {
+        type: "input",
+        name: "test",
+        message:"Please provide any testing information if there is any",
         
-        },
-        {
-            type:"input",
-            name:"test",
-            message:"Please provide command for any test to be run",
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Please define any contributions made by the user or other developers",
+    }])
+
+    .then(response => {
         
-        },
-        {   type:"input",
-             name:"contributing",
-            message:"Does the user need to contrubte to the repo?",
 
-        },
-
-
-    
-//console.log everything 
-
- 
-//how to use the data that the user input
-var getInput = (data) => {
-
-}
-
-
-
-
-// function to generate markdown for README// from the starter code:figure out what they do
-function generateMarkdown(data) {
-    return `# ${data.title}
-    ##Description
-    ${data.description}
-    ## Tabel of Contents
-    - [Questions](#questions)
-    -[License](#license)
-    - []
-  
-  `;
-  }
-  
-  module.exports = generateMarkdown;
-
+    })
 
 
